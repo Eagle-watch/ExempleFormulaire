3b06134b3d9a52a14f9f453af3f6eba9ad2360a0
54bryan.francois <b.francois.gj@gmail.com>
2022-10-11 12:01:05 +0200

    Initial commit

---
+++ src/main/java/FenetrePrincipale.java
@@ -0,0 +1,75 @@
+import javax.swing.*;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import java.awt.*;
+
+public class FenetrePrincipale extends JFrame {
+
+    protected boolean themeSombreActif = true;
+    protected int defaultMargin = 10;
+
+    public FenetrePrincipale() {
+        setSize(500,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //ajout du panneau principal avec un layout de 5 zones
+        // (NORTH, SOUTH, EAST, WEST, CENTER)
+        JPanel panneau = new JPanel(new BorderLayout());
+        setContentPane(panneau);
+
+        //--------- BOUTON THEME ------------------------
+
+        JButton boutonTheme = new JButton("Changer le theme");
+
+
+        boutonTheme.addActionListener(
+                e -> {
+                    try {
+                        if(themeSombreActif) {
+                            UIManager.setLookAndFeel(new FlatLightLaf());
+                        } else {
+                            UIManager.setLookAndFeel(new FlatDarculaLaf());
+                        }
+                        SwingUtilities.updateComponentTreeUI(this);
+                        themeSombreActif = !themeSombreActif;
+
+                    } catch (UnsupportedLookAndFeelException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+        );
+
+        //--------- BOUTON VALIDER FORMULAIRE -----------
+
+        JButton boutonValider = new JButton("Enregistrer");
+
+        boutonValider.addActionListener(e -> System.out.println("Formulaire validé"));
+        boutonValider.setSize(new Dimension(100, 30));
+
+        //---------- DISPOSITION DES COMPOSANTS ---------
+
+        panneau.add(HelperForm.generateRow(boutonTheme,10 , 10 , 0 ,0 , HelperForm.ALIGN_RIGHT), BorderLayout.NORTH);
+
+        Box containerZoneBoutonAction = Box.createVerticalBox();
+        panneau.add(containerZoneBoutonAction, BorderLayout.SOUTH);
+
+        Box zoneBoutonAction = Box.createHorizontalBox();
+        containerZoneBoutonAction.add(zoneBoutonAction);
+        containerZoneBoutonAction.add(
+                Box.createRigidArea(new Dimension(1,defaultMargin)));
+
+        zoneBoutonAction.add(Box.createHorizontalGlue());
+        zoneBoutonAction.add(boutonValider);
+        zoneBoutonAction.add(Box.createRigidArea(
+                new Dimension(defaultMargin,1)));
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        FlatDarculaLaf.setup();
+        new FenetrePrincipale();
+    }
+
+
+}
